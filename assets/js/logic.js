623c5bdf009798d6b8e8ada69ea9d634c720d3e7
rachel esteve <rachiraesteve@yahoo.com>
2023-12-24 20:49:59 +0000

    add start button click event with timer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,3 +6,13 @@ const scoreDisplay = document.getElementById('final-score')
 
 let currentQuestion = 0
 let time = 75
+
+startButton.addEventListener('click', function() {
+  document.getElementById('start-screen').classList.add('hide')
+  questionsContainer.classList.remove('hide')
+  timeDisplay.innerHTML = time
+  setInterval(function() {
+    time--
+    timeDisplay.innerHTML = time
+  }, 1000)
+})
