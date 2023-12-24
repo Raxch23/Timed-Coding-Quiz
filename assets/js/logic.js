3cb761a2892f6aa5bdd73c42eeb3bb0a7f1e45e7
rachel esteve <rachiraesteve@yahoo.com>
2023-12-24 20:47:14 +0000

    get interface elements into javascript variables

---
+++ assets/js/logic.js
@@ -0,0 +1,5 @@
+const startButton = document.getElementById('start')
+const questionsContainer = document.getElementById('questions')
+const endScreen = document.getElementById('end-screen')
+const timeDisplay = document.getElementById('time')
+const scoreDisplay = document.getElementById('final-score')
