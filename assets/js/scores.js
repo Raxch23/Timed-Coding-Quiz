3fca9eaabf0fcfe5b5677d3253790e10981d443a
rachel esteve <rachiraesteve@yahoo.com>
2023-12-24 21:14:00 +0000

    clear highscores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,1 +1,6 @@
 const highscoresDisplay = document.getElementById('highscores')
+
+const clearHighscoresButton = document.getElementById('clear')
+clearHighscoresButton.addEventListener('click', function() {
+  highscoresDisplay.innerHTML = ''
+})
