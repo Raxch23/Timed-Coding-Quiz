f7b49fc97f267fc40cec9cf9c00fb6bb1cce9302
rachel esteve <rachiraesteve@yahoo.com>
2023-12-24 20:48:03 +0000

    add initial variables time and question

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,3 +3,6 @@ const questionsContainer = document.getElementById('questions')
 const endScreen = document.getElementById('end-screen')
 const timeDisplay = document.getElementById('time')
 const scoreDisplay = document.getElementById('final-score')
+
+let currentQuestion = 0
+let time = 75
