b3a8b41b779d39b7597da687f6fe5e0292955ff2
rachel esteve <rachiraesteve@yahoo.com>
2023-12-24 21:01:11 +0000

    load question data iunto quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -9,6 +9,7 @@ let time = 75
 
 startButton.addEventListener('click', function() {
   document.getElementById('start-screen').classList.add('hide')
+  loadQuestion(currentQuestion)
   questionsContainer.classList.remove('hide')
   timeDisplay.innerHTML = time
   setInterval(function() {
@@ -16,3 +17,12 @@ startButton.addEventListener('click', function() {
     timeDisplay.innerHTML = time
   }, 1000)
 })
+
+function loadQuestion(currentQuestion) {
+  questionsContainer.children[0].innerHTML = questions[currentQuestion].title
+  for (const [optionIndex, option] of questions[currentQuestion].choices.entries()) {
+    const optionElement = document.createElement('button')
+    optionElement.innerHTML = (optionIndex + 1) + '. ' + option
+    questionsContainer.children[1].appendChild(optionElement)
+  }  
+}
