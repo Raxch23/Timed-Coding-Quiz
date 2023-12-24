dc82e44571b2d797f80fca95635626dd1aa0f049
rachel esteve <rachiraesteve@yahoo.com>
2023-12-24 21:00:44 +0000

    add questions file

---
+++ assets/js/questions.js
@@ -0,0 +1,47 @@
+const questions = [
+  {
+    title: 'What does HTML stand for?',
+    choices: [
+      'Hyper Text Markup Language',
+      'Hyper Transfer Markup Language',
+      'High-level Textual Markup Language',
+    ],
+    answer: 0
+  },
+  {
+    title: 'Which HTML tag is used to create a hyperlink?',
+    choices: [
+      'link',
+      'a',
+      'hlink',
+    ],
+    answer: 1
+  },
+  {
+    title: 'Which HTML element is used to define the structure of an HTML document, including the head and body sections?',
+    choices: [
+      'section',
+      'document',
+      'html',
+    ],
+    answer: 2
+  },
+  {
+    title: 'What is the purpose of the HTML img tag?',
+    choices: [
+      'Insert a video',
+      'Embed an image',
+      'Create a hyperlink',
+    ],
+    answer: 1
+  },
+  {
+    title: 'Which HTML attribute is used to provide additional information about an element and is often displayed as a tooltip when the mouse hovers over the element?',
+    choices: [
+      'title',
+      'info',
+      'tooltip',
+    ],
+    answer: 0
+  },
+]
