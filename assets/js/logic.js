4eec88366174f790d9ca36ca9b1786df37de5acd
rachel esteve <rachiraesteve@yahoo.com>
2023-12-24 21:10:12 +0000

    stop timer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,6 +6,7 @@ const scoreDisplay = document.getElementById('final-score')
 
 let currentQuestion = 0
 let time = 75
+let timer = undefined
 
 const resultDisplay = document.createElement('div')
 questionsContainer.appendChild(resultDisplay)
@@ -15,7 +16,7 @@ startButton.addEventListener('click', function() {
   loadQuestion(currentQuestion)
   questionsContainer.classList.remove('hide')
   timeDisplay.innerHTML = time
-  setInterval(function() {
+  timer = setInterval(function() {
     time--
     timeDisplay.innerHTML = time
   }, 1000)
@@ -41,6 +42,8 @@ function loadQuestion(currentQuestion) {
       } else {
         questionsContainer.classList.add('hide')
         endScreen.classList.remove('hide')
+        clearInterval(timer)
+        scoreDisplay.innerHTML = time
       }
     })
     questionsContainer.children[1].appendChild(optionElement)
